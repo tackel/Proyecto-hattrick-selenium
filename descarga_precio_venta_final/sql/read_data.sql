e7fcde0936d0cab9697a446e4a468809dbc848fc
Fernando <74513344+tackel@users.noreply.github.com>
2022-10-04 02:03:56 -0300

    ventas

--- descarga_precio_venta_final/sql/read_data.sql
+++ descarga_precio_venta_final/sql/read_data.sql
@@ -1,4 +1,4 @@
 SELECT Nombre, link, Edad, Días from players
 WHERE Precio_venta IS NULL and strftime("%Y%m%d","now") > substr(Límite, 7, 4) || "" || substr(Límite,4,2) || "" || substr(Límite, 1,2)
 ORDER by Id ASC
-LIMIT 400
+LIMIT 500
